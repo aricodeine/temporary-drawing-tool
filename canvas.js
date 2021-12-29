d485aad68b71eac38817f193542339d1145f0cc2
aricodeine <88302441+aricodeine@users.noreply.github.com>
2021-12-29 13:39:12 +0530

    Update canvas.js

--- canvas.js
+++ canvas.js
@@ -1,3 +1,42 @@
+const pickr = Pickr.create({
+    el: '.color-picker',
+    theme: 'classic', // or 'monolith', or 'nano'
+    default: '#000000',
+    swatches: [
+        'rgba(244, 67, 54, 1)',
+        'rgba(233, 30, 99, 0.95)',
+        'rgba(156, 39, 176, 0.9)',
+        'rgba(103, 58, 183, 0.85)',
+        'rgba(63, 81, 181, 0.8)',
+        'rgba(33, 150, 243, 0.75)',
+        'rgba(3, 169, 244, 0.7)',
+        'rgba(0, 188, 212, 0.7)',
+        'rgba(0, 150, 136, 0.75)',
+        'rgba(76, 175, 80, 0.8)',
+        'rgba(139, 195, 74, 0.85)',
+        'rgba(205, 220, 57, 0.9)',
+        'rgba(255, 235, 59, 0.95)',
+        'rgba(255, 193, 7, 1)'
+    ],
+
+    components: {
+
+        // Main components
+        preview: true,
+        opacity: true,
+        hue: true,
+
+        // Input / output Options
+        interaction: {
+            hex: true,
+            rgba: true,
+            input: true,
+            // clear: true,
+            save: true
+        }
+    }
+});
+
 window.addEventListener('load', () => {
     const canvas = document.querySelector('#canvas');
     const ctx = canvas.getContext('2d');
@@ -5,8 +44,11 @@ window.addEventListener('load', () => {
     let canvasDataIndex = -1;
     let painting = false;
     let fontSize = "2";
-    let draw_color = "black";
 
+    
+
+    let draw_color = "black";
+    
     canvas.width = window.innerWidth - 400;
     canvas.height = window.innerHeight - 100;
     ctx.fillStyle = "whitesmoke";
@@ -56,8 +98,9 @@ window.addEventListener('load', () => {
         fontSize = document.querySelector('.pen-range').value;
     });
 
-    document.querySelector('.color-picker').addEventListener('input', () => {
-        draw_color = document.querySelector('.color-picker').value;
+    pickr.on('change', (color) => {
+        // document.querySelector('.pickr .pcr-butto::after').style.setProperty('background', 'var(color.toRGBA().toString())');
+        draw_color = color.toRGBA().toString();
     });
 
     canvas.addEventListener("mousedown", startPosition);
